4f61d5f238506f3aedf7417cb8efd0980971cf2d
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-11 15:24:03 -0600

    trying out asteroid carousel (not showing up yet but learning the different customizations using the documentation)

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,12 +1,18 @@
 import Starfield from "./components/Starfield";
 import Footer from "./components/Footer"
+import AsteroidCarousel from "./components/AsteroidCarousel";
+
 function App() {
   return (
     <div className="fixed w-screen h-screen bg-midnight text-white flex font-sans justify-center">
       <Starfield/>
       
       <h1 className="m-5 text-4xl font-serif">Asteroid Watcher</h1>
-
+      
+      <div className="flex bg-pink items-center justify-center z-10 min-h-screen">
+        <AsteroidCarousel/>
+      </div>
+      
       <Footer/>
     </div>
   );
--- space-app/src/components/AsteroidCarousel.jsx
+++ space-app/src/components/AsteroidCarousel.jsx
@@ -1,3 +1,43 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import React from 'react';
 
+const responsive = {
+  all: {
+    breakpoint: { max: 3000, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
+
+const auto = false;
+
+function AsteroidCarousel({  }) {
+  return (
+    <div>
+      <Carousel
+        swipeable={true}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlay={auto}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <div className="p-10 bg-white text-black">Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default AsteroidCarousel;
